cba33f5e5fbca990b5044a770c1a12ea6f5d6a09
Tyler Rose <trose@localhost.localdomain>
2018-11-25 15:28:52 -0500

    Parsing now works...although not in parallel

--- src/DataParse.cu
+++ src/DataParse.cu
@@ -1,14 +1,33 @@
 #include <fstream>
 #include <map>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <vector>
 
-/*int getBit(unsigned char *bytes, int bit) {
+#define CUDA_ERROR_CHECK
+
+#define CudaSafeCall( err ) __cudaSafeCall( err, __FILE__, __LINE__ )
+
+inline void __cudaSafeCall( cudaError err, const char *file, const int line )
+{
+#ifdef CUDA_ERROR_CHECK
+    if ( cudaSuccess != err )
+    {
+        fprintf( stderr, "cudaSafeCall() failed at %s:%i : %s\n",
+                 file, line, cudaGetErrorString( err ) );
+        exit( -1 );
+    }
+#endif
+
+    return;
+}
+
+int getBit(unsigned char *bytes, int bit) {
     return ((bytes[(bit/8)] >> (bit % 8)) & 1);
 }
 
-__device__ void setBit(unsigned char *bytes, int bit, int val) {
+/*__device__ void setBit(unsigned char *bytes, int bit, int val) {
     if (val == 1)
         bytes[(bit/8)] |= (1 << (bit % 8));
     else
@@ -23,39 +42,127 @@ void printBits(unsigned char *ptr, int sizeInBytes) {
 }*/
 
 __global__ void description_to_tags(char **d_descs, unsigned char *d_results, int sizeEntries, char **d_tags, int sizeTags) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    /*if (idx < sizeTags) {
-        //c_tags[idx] = d_tags[idx];
-        printf("Thread %d: %s\n", idx, d_tags[idx]);
-    }*/
-    if (idx >= sizeEntries)
+    if (threadIdx.x > 0) 
         return;
-    //char* desc = d_descs[idx];
-    printf("Here with thread for idx: %d, %s\n", idx, d_descs[0]);
-    //for (char *tag = *d_tags; tag; tag = *++d_tags) {
-    for (int i = 0; i < sizeTags; i++) {
-        /*char* tag = c_tags[i];
-        //Check if the description contains the tag
-        printf("%d: %s\n", idx, tag);
-        while (*desc) {
+    for (int num = 0; num < sizeTags; num++) {
+        for (int i = 0; i < sizeEntries; i++) {
+            int spacing = sizeEntries/sizeTags;
+            int idx = (i + threadIdx.x*spacing) % sizeEntries;
+
+            // Copy the desc locally so we don't have read conflicts
+            int j = 0;
+            char desc[2000];
+            while (d_descs[idx][j] != '\0') {
+                desc[j] = d_descs[idx][j];
+                j++;
+            }
+            desc[j] = '\0';
+
+            //char* tag = d_tags[threadIdx.x];
+            char* tag = d_tags[num];
+            int tagLength = 0;
+            while (tag[tagLength] != '\0')
+                tagLength++;
             
-        }*/
-        printf("Thread %d: %s\n", idx, d_tags[i]);
+            int match = 0;
+            bool positiveMatch = false;
+            j = 0;
+            while (desc[j] != '\0') {
+                char descLetter = desc[j];
+                if ('A'<=descLetter && descLetter<='Z'){
+                    descLetter=char(((int)descLetter)+32);
+                }
+
+                if (descLetter == tag[match]) {
+                    match++;
+                    if (match == tagLength) {
+                        positiveMatch = true;
+                        break;
+                    }
+                }
+                else
+                    match = 0;
+                j++;
+            }
+            
+            if (positiveMatch) {
+                //printf("Thread %d:%d is looking for %s\t%s\n", threadIdx.x, idx, tag, "True!");
+                // Since 11 bytes are given for each entry we need to find the byte that we are in and add the specific flag we need
+                switch (num % 8) {
+                    case 0:
+                        d_results[(num/8) * i] |= 0b10000000;
+                        break;
+                    case 1:
+                        d_results[(num/8) * i] |= 0b01000000;
+                        break;
+                    case 2:
+                        d_results[(num/8) * i] |= 0b00100000;
+                        break;
+                    case 3:
+                        d_results[(num/8) * i] |= 0b00010000;
+                        break;
+                    case 4:
+                        d_results[(num/8) * i] |= 0b00001000;
+                        break;
+                    case 5:
+                        d_results[(num/8) * i] |= 0b00000100;
+                        break;
+                    case 6:
+                        d_results[(num/8) * i] |= 0b00000010;
+                        break;
+                    case 7:
+                        d_results[(num/8) * i] |= 0b00000001;
+                        break;
+                }
+            }
+            else {
+                //printf("Thread %d:%d is looking for %s\t%s\n", threadIdx.x, idx, tag, "False");
+                switch (num % 8) {
+                    case 0:
+                        d_results[(num/8) * i] &= ~0b10000000;
+                        break;
+                    case 1:
+                        d_results[(num/8) * i] &= ~0b01000000;
+                        break;
+                    case 2:
+                        d_results[(num/8) * i] &= ~0b00100000;
+                        break;
+                    case 3:
+                        d_results[(num/8) * i] &= ~0b00010000;
+                        break;
+                    case 4:
+                        d_results[(num/8) * i] &= ~0b00001000;
+                        break;
+                    case 5:
+                        d_results[(num/8) * i] &= ~0b00000100;
+                        break;
+                    case 6:
+                        d_results[(num/8) * i] &= ~0b00000010;
+                        break;
+                    case 7:
+                        d_results[(num/8) * i] &= ~0b00000001;
+                        break;
+                }
+            }
+            __syncthreads();
+        }
     }
 }
 
 std::map<std::string, std::vector<float> > dataConversion(std::map<std::string, std::vector<std::string> > rawData, std::vector<std::string> tags_internal) {
-    //const char** descData = new const char*[2000];
     const char** descData = (const char**) malloc(sizeof(char)*2000*rawData.size());
+    // Collected data is other data that we need to return we collect it so we don't need to iterate through the map again and worry about order.
+    // The data is the style of the beer, abv, ibu
+    std::vector< std::vector<std::string> > collectedData;
     int i = 0;
     // Put desc data into char**
     for (std::map<std::string, std::vector<std::string> >::iterator it = rawData.begin(); it != rawData.end(); it++) {
-        /*printf("%s: ", it->first.c_str());
-        for (uint i = 0; i < it->second.size(); i++) {
-            printf("%s - ", it->second[i].c_str());
-        }
-        printf("\n");*/
         descData[i] = it->second[1].c_str();
+        std::vector<std::string> data;
+        data.push_back(it->second[4].c_str());
+        data.push_back(it->second[2].c_str());
+        data.push_back(it->second[3].c_str());
+        collectedData.push_back(data);
         i++;
     }
 
@@ -70,50 +177,56 @@ std::map<std::string, std::vector<float> > dataConversion(std::map<std::string, 
     /* Since we have 11Gb of memory on my GPU we don't need to worry about memory...at 
     85*20 bytes for the tags, 2000 bytes per beer for description, 11 bytes per beer for results
     it would take around 5.5 million beers to run out of memory...We don't have that*/
-    const dim3 blockSize(1024, 1, 1);
-    const dim3 gridSize(ceil(rawData.size()/1024.0), 1, 1);
+    const dim3 blockSize(tags_internal.size(), 1, 1);
+    const dim3 gridSize(1, 1, 1);
     
     char **d_descs, **d_tags;
-    unsigned char *d_results, *parsedResults;
+    unsigned char *d_results;
     
-    cudaMalloc(&d_results, 11*rawData.size());
-    /*cudaMalloc(&d_tags, tags_internal.size()*20);
-    cudaMemcpy(d_tags, &tags, tags_internal.size()*20, cudaMemcpyHostToDevice);
-    //cudaMemcpyToSymbol(c_tags, &tags, tags_internal.size()*20, 0, cudaMemcpyHostToDevice);
-    cudaMemcpyToSymbol("c_tags", d_tags, tags_internal.size()*20, 0, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_descs, descData, rawData.size()*2000, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_results, 0, 11*rawData.size(), cudaMemcpyHostToDevice);*/
+    unsigned char *parsedResults = (unsigned char*) malloc(11*rawData.size());
+    CudaSafeCall(cudaMalloc(&d_results, 11*rawData.size()));
 
     // Copy descs to device
-    cudaMalloc(&d_descs, rawData.size()*sizeof(char*));
+    CudaSafeCall(cudaMalloc(&d_descs, rawData.size()*sizeof(char*)));
     char **d_temp_desc;
     d_temp_desc = (char **)malloc(rawData.size()*sizeof(char *));
     for (int i = 0; i < rawData.size(); i++){
-        cudaMalloc(&(d_temp_desc[i]), 2000*sizeof(char));
-        cudaMemcpy(d_temp_desc[i], descData[i], 2000*sizeof(char), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_descs+i, &(d_temp_desc[i]), sizeof(char *), cudaMemcpyHostToDevice);
+        CudaSafeCall(cudaMalloc(&(d_temp_desc[i]), 2000*sizeof(char)));
+        CudaSafeCall(cudaMemcpy(d_temp_desc[i], descData[i], 2000*sizeof(char), cudaMemcpyHostToDevice));
+        CudaSafeCall(cudaMemcpy(d_descs+i, &(d_temp_desc[i]), sizeof(char *), cudaMemcpyHostToDevice));
     }
     free(d_temp_desc);
-    
-    // Copy tags to constant memory
-    cudaMalloc(&d_tags, tags_internal.size()*sizeof(char*));
+
+    // Copy tags to global memory
+    CudaSafeCall(cudaMalloc(&d_tags, tags_internal.size()*sizeof(char*)));
     char **d_temp_tags;
     d_temp_tags = (char **)malloc(tags_internal.size()*sizeof(char*));
     for (int i = 0; i < tags_internal.size(); i++) {
-        cudaMalloc(&(d_temp_tags[i]), 20*sizeof(char));
-        cudaMemcpy(d_temp_tags[i], tags[i], 20*sizeof(char), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_tags+i, &(d_temp_tags[i]), sizeof(char *), cudaMemcpyHostToDevice);
+        CudaSafeCall(cudaMalloc(&(d_temp_tags[i]), 20*sizeof(char)));
+        CudaSafeCall(cudaMemcpy(d_temp_tags[i], tags[i], 20*sizeof(char), cudaMemcpyHostToDevice));
+        CudaSafeCall(cudaMemcpy(d_tags+i, &(d_temp_tags[i]), sizeof(char *), cudaMemcpyHostToDevice));
     }
     free(d_temp_tags);
 
-    description_to_tags<<<gridSize, blockSize>>>(d_descs, d_results, rawData.size(), d_tags, tags_internal.size());//, d_results, rawData.size(), d_tags);
+    description_to_tags<<<gridSize, blockSize>>>(d_descs, d_results, rawData.size(), d_tags, tags_internal.size());
+
+    CudaSafeCall(cudaMemcpy(parsedResults, d_results, 11*rawData.size(), cudaMemcpyDeviceToHost));
+
+    for (int i = 0; i < rawData.size(); i++) {
+        std::vector<float> entry;
+        // Put tag results into vector
+        for (int j = 0; j < 88; j++) {
+            entry.push_back((float) getBit(parsedResults, i*88 + j));
+            //printf("entry[-1]);
+        }
+        // Add abv and ibu
+        entry.push_back(atof(collectedData[i][1].c_str()));
+        entry.push_back(atof(collectedData[i][2].c_str()));
+        results[collectedData[i][0]] = entry;
+    }
     
-    //parsedResults = (unsigned char*) malloc(11*rawData.size());
-    //cudaMemcpy(parsedResults, d_results, 11*rawData.size(), cudaMemcpyDeviceToHost);
-    //delete descData;
-    ////cudaFree(c_tags);
-    cudaFree(d_descs);
-    cudaFree(d_results);
+    CudaSafeCall(cudaFree(d_descs));
+    CudaSafeCall(cudaFree(d_results));
     free(parsedResults);
     free(descData);
     free(tags);
--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -4,5 +4,6 @@
 
 int main(int argc, char *argv[]) {
 	Data* data = new Data();
+	data->print();
 	delete data;
 }
